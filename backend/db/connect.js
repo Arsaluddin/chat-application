80aba44d2af41ac62564b4a6f58d4a369a0e7426
arsal uddin <arsaluddin134@gmail.com>
2023-08-29 17:22:08 +0530

    .

--- backend/db/connect.js
+++ backend/db/connect.js
@@ -1,8 +1,7 @@
 // backend/config/db.js
 const mongoose = require('mongoose');
 // 
-// MONGODB_URI = mongodb+srv://arsaluddin134:VyaSXfFBOX3Mb8Aj@cluster0.mcc8iiu.mongodb.net
-// PORT = 5000
+
 
 // Replace this URL with your MongoDB Atlas connection string
 
